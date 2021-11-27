60ace46288dcce49c7caa30aa47df8d2e7ccdc39
bayaroch <bayaroch@gmail.com>
2021-11-27 23:29:17 +0800

    fix

--- src/constants/menu.constants.ts
+++ src/constants/menu.constants.ts
@@ -10,23 +10,27 @@ export interface MenuItemType {
 }
 
 export const primarymenu: MenuItemType[] = [
-  {
-    label: 'Бидний тухай',
-    route: 'about',
-    id: 1,
-    slug: 'about',
-  },
-  {
-    label: 'Хамтран ажиллах',
-    route: 'partner',
-    id: 2,
-    slug: 'partner',
-  },
+  // {
+  //   label: 'Бидний тухай',
+  //   route: 'about',
+  //   id: 1,
+  //   slug: 'about',
+  // },
+  // {
+  //   label: 'Хамтран ажиллах',
+  //   route: 'partner',
+  //   id: 2,
+  //   slug: 'partner',
+  // },
   {
     label: 'Холбоо Барих',
     route: 'contact',
     id: 3,
     slug: 'contact',
+    seo: {
+      image: 'images/default.png',
+      content: 'Холбоо Барих',
+    },
   },
   {
     label: 'Үйлчилгээний нөхцөл',
