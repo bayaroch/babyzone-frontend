a191ffd0eacf1537b4858f2d3826c4c707edbc90
bayaroch <bayaroch@gmail.com>
2021-11-28 21:20:54 +0800

    fix

--- pages/gender_chart/useGenderForm.ts
+++ pages/gender_chart/useGenderForm.ts
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { useForm, Controller } from 'react-hook-form'
-import { useMemo } from 'react'
-import * as yup from 'yup'
-import { yupResolver } from '@hookform/resolvers/yup'
 
 export const initValues = {
   av: '18',
@@ -15,17 +12,7 @@ export type GenderFormParams = {
 }
 
 const useCreateForm = () => {
-  const validationSchema = useMemo(
-    () =>
-      yup.object().shape({
-        av: yup.string().required('Насаа сонго'),
-        mv: yup.string().required('Сараа сонго'),
-      }),
-    []
-  )
-
   const methods = useForm<GenderFormParams>({
-    resolver: yupResolver(validationSchema),
     defaultValues: initValues,
   })
 
