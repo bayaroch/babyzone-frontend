bdd939f2e6fddf03e666039bcfa5d289a7e8f9b0
Joy Spark <bayaroch@gmail.com>
2024-08-12 20:19:20 +0800

    fix

--- pages/page/[slug]/index.tsx
+++ pages/page/[slug]/index.tsx
@@ -1,51 +1,71 @@
-/*
- * Detail Page
- */
+import React from 'react'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+import { Container, Grid, Typography } from '@mui/material'
+import _ from 'lodash'
+
 import MainLayout from '@components/Layouts/MainLayout'
 import PageWithLayoutType from '@constants/page'
-import { useRouter } from 'next/router'
-import { Container, Grid, Typography } from '@mui/material'
 import CategoryMenu from '@containers/CategoryMenu'
-import usePage from '@utils/hooks/usePage'
-import { useEffect } from 'react'
 import Content from '@components/Content'
 import Loader from '@components/Loader'
-import _ from 'lodash'
 import Seo from '@components/Seo'
 import { primarymenu } from '@constants/menu.constants'
+import { pageServices } from '@services/page.services'
+import { WP_REST_API_Post } from 'wp-types'
 
 const Terms: PageWithLayoutType = () => {
   const router = useRouter()
   const { slug } = router.query
 
-  const { detail, initPage, meta } = usePage()
+  const fetchPage = async (
+    slug: string
+  ): Promise<WP_REST_API_Post | undefined> => {
+    const data = await pageServices.page(slug)
+    return data[0] // Assuming we want the first page if multiple are returned
+  }
+
+  const { data: detail, status, error, refetch } = useQuery(
+    ['page', slug],
+    () => fetchPage(slug as string),
+    {
+      enabled: !!slug,
+    }
+  )
 
   const seo = _.find(primarymenu, { slug: slug as string })
 
-  useEffect(() => {
-    if (slug) initPage(slug as string)
-  }, [slug])
+  React.useEffect(() => {
+    if (slug) {
+      refetch()
+    }
+  }, [slug, refetch])
 
   const renderContent = () => {
-    if (detail !== undefined && meta.loaded && !meta.pending) {
-      return (
-        <>
-          <Typography
-            mb={1}
-            variant="h1"
-            align="center"
-            sx={{
-              padding: 1,
-              fontSize: {
-                lg: 28,
-                md: 20,
-                sm: 18,
-                xs: 16,
-              },
-            }}
-          >
-            {detail.title.rendered}
-          </Typography>
+    if (!detail) return null
+
+    return (
+      <>
+        <Typography
+          mb={1}
+          variant="h1"
+          align="center"
+          sx={{
+            padding: 1,
+            fontSize: {
+              lg: 28,
+              md: 20,
+              sm: 18,
+              xs: 16,
+            },
+          }}
+        >
+          {detail.title.rendered}
+        </Typography>
+        {_.get(
+          detail,
+          "_embedded['wp:featuredmedia'][0].media_details.sizes.full.source_url"
+        ) && (
           <img
             style={{ width: '100%', height: 'auto' }}
             src={
@@ -55,37 +75,26 @@ const Terms: PageWithLayoutType = () => {
                 ''
               ) as string
             }
+            alt={detail.title.rendered}
           />
-          <Content
-            sx={{
-              padding: {
-                xs: 2,
-                md: 1,
-                lg: 4,
-              },
-              paddingTop: {
-                sm: 0,
-              },
-            }}
-            content={detail.content.rendered}
-          />
-        </>
-      )
-    }
-  }
-  const renderLoader = () => {
-    if (meta.pending) {
-      return <Loader />
-    }
+        )}
+        <Content
+          sx={{
+            padding: {
+              xs: 2,
+              md: 1,
+              lg: 4,
+            },
+            paddingTop: {
+              sm: 0,
+            },
+          }}
+          content={detail.content.rendered}
+        />
+      </>
+    )
   }
 
-  const renderNotFound = () => {
-    if (!meta.pending && meta.loaded && detail === undefined) {
-      return <Typography>Хуудас олдсонгүй</Typography>
-    }
-  }
-
-  // eslint-disable-next-line no-console
   return (
     <MainLayout>
       {seo && (
@@ -106,9 +115,16 @@ const Terms: PageWithLayoutType = () => {
       >
         <Grid container columnSpacing={{ xs: 0 }}>
           <Grid item xs={12} md={8}>
-            {renderContent()}
-            {renderLoader()}
-            {renderNotFound()}
+            {status === 'loading' && <Loader />}
+            {status === 'error' && (
+              <Typography>Error: {(error as Error).message}</Typography>
+            )}
+            {status === 'success' &&
+              (detail ? (
+                renderContent()
+              ) : (
+                <Typography>Хуудас олдсонгүй</Typography>
+              ))}
           </Grid>
           <Grid md={4} item>
             <CategoryMenu category={undefined} />
